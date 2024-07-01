fc1896b36a188ebc44110134d9f5060b32a72074
maxsudbek-mirzabekov <maxsudbekmirzabekov7@gmail.com>
2024-07-01 17:01:35 +0500

    maxsudbek mirzabekov

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Maxsudbek from './maxxsudbek/Maxsudbek'
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Maxsudbek />
+    </div>
+  );
 }
 
 export default App;
